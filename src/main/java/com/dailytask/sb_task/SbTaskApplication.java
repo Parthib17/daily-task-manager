8ab3c9c2713febb0e519a43e35f28f364a6abaed
Parthib <parthibmitra1278@gmail.com>
2025-12-31 14:54:10 +0530

    Initial setup

---
+++ src/main/java/com/dailytask/sb_task/SbTaskApplication.java
@@ -0,0 +1,13 @@
+package com.dailytask.sb_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailytask/sb_task/SbTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailytask.sb_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
